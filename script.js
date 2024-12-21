99a28c78f2f8e28529ace115b825e773a016cfdf
devshimul <shimul.bisd@gmail.com>
2024-12-21 15:18:27 +0600

    function and object

---
+++ script.js
@@ -0,0 +1,23 @@
+// function sum() {
+//   console.log("Hello from js function");
+// }
+// console.log(sum());
+
+// let name = "Jolil";
+// let age = 20;
+// // console.log("I am age Years old");
+// // console.log(`I am ${age} Years old`);
+// console.log(`I am ${name} and, I am ${age} years old`);
+// function added(num1, num2) {
+//   const total = num1 + num2;
+//   return total;
+// }
+// const sum = added(445, 45);
+// console.log(sum);
+
+function appleOrangeJuice(apples, oranges, papaya) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges. Also make juice with ${papaya} papaya`;
+  return juice;
+}
+const juice = appleOrangeJuice(1, 2, 1);
+console.log(juice);
